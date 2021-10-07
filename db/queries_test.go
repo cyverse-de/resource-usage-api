ab62d694f48fab58b6a11d6ab36b70a31361f4f1
John Wregglesworth <wregglej@cyverse.org>
2021-10-07 14:48:33 -0700

    Simplify query tests

--- db/queries_test.go
+++ db/queries_test.go
@@ -8,39 +8,37 @@ import (
 	"go.uber.org/multierr"
 )
 
+var queries = map[string]string{
+	"analysisIDByExternalIDQuery":          analysisIDByExternalIDQuery,
+	"analysisQuery":                        analysisQuery,
+	"currentCPUHoursForUserQuery":          currentCPUHoursForUserQuery,
+	"updateCurrentCPUHoursForUserQuery":    updateCurrentCPUHoursForUserQuery,
+	"allCPUHoursForUserQuery":              allCPUHoursForUserQuery,
+	"currentCPUHoursQuery":                 currentCPUHoursQuery,
+	"allCPUHoursQuery":                     allCPUHoursQuery,
+	"insertCPUHourEventStmt":               insertCPUHourEventStmt,
+	"unprocessedEventsQuery":               unprocessedEventsQuery,
+	"claimedByStmt":                        claimedByStmt,
+	"processingStmt":                       processingStmt,
+	"finishedProcessingStmt":               finishedProcessingStmt,
+	"registerWorkerStmt":                   registerWorkerStmt,
+	"unregisterWorkerStmt":                 unregisterWorkerStmt,
+	"refreshWorkerStmt":                    refreshWorkerStmt,
+	"purgeExpiredWorkersStmt":              purgeExpiredWorkersStmt,
+	"purgeExpiredWorkSeekersStmt":          purgeExpiredWorkSeekersStmt,
+	"purgeExpiredWorkClaimsStmt":           purgeExpiredWorkClaimsStmt,
+	"resetWorkClaimForInactiveWorkersStmt": resetWorkClaimForInactiveWorkersStmt,
+	"gettingWorkStmt":                      gettingWorkStmt,
+	"notGettingWorkStmt":                   notGettingWorkStmt,
+	"setWorkingStmt":                       setWorkingStmt,
+}
+
 func TestSQLSyntax(t *testing.T) {
 	var err error
 
-	queries := []map[string]string{
-		{"analysisIDByExternalIDQuery": analysisIDByExternalIDQuery},
-		{"analysisQuery": analysisQuery},
-		{"currentCPUHoursForUserQuery": currentCPUHoursForUserQuery},
-		{"updateCurrentCPUHoursForUserQuery": updateCurrentCPUHoursForUserQuery},
-		{"allCPUHoursForUserQuery": allCPUHoursForUserQuery},
-		{"currentCPUHoursQuery": currentCPUHoursQuery},
-		{"allCPUHoursQuery": allCPUHoursQuery},
-		{"insertCPUHourEventStmt": insertCPUHourEventStmt},
-		{"unprocessedEventsQuery": unprocessedEventsQuery},
-		{"claimedByStmt": claimedByStmt},
-		{"processingStmt": processingStmt},
-		{"finishedProcessingStmt": finishedProcessingStmt},
-		{"registerWorkerStmt": registerWorkerStmt},
-		{"unregisterWorkerStmt": unregisterWorkerStmt},
-		{"refreshWorkerStmt": refreshWorkerStmt},
-		{"purgeExpiredWorkersStmt": purgeExpiredWorkersStmt},
-		{"purgeExpiredWorkSeekersStmt": purgeExpiredWorkSeekersStmt},
-		{"purgeExpiredWorkClaimsStmt": purgeExpiredWorkClaimsStmt},
-		{"resetWorkClaimForInactiveWorkersStmt": resetWorkClaimForInactiveWorkersStmt},
-		{"gettingWorkStmt": gettingWorkStmt},
-		{"notGettingWorkStmt": notGettingWorkStmt},
-		{"setWorkingStmt": setWorkingStmt},
-	}
-
-	for _, q := range queries {
-		for name, query := range q {
-			if _, perr := parser.Parse(query); perr != nil {
-				err = multierr.Append(err, fmt.Errorf("error parsing %s: %w", name, perr))
-			}
+	for name, query := range queries {
+		if _, perr := parser.Parse(query); perr != nil {
+			err = multierr.Append(err, fmt.Errorf("error parsing %s: %w", name, perr))
 		}
 	}
 
