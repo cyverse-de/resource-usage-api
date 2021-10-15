e446442cc8de8ecdc42b0b71dce83ccb5ee3cfa7
John Wregglesworth <wregglej@cyverse.org>
2021-10-15 12:04:44 -0700

    Clean up analyses.go

--- db/analyses.go
+++ db/analyses.go
@@ -18,46 +18,49 @@ type Analysis struct {
 	SysteMID   string
 }
 
-const analysisIDByExternalIDQuery = `
-	SELECT j.id
-	FROM jobs j
-	JOIN job_steps s ON s.job_id = j.id
-	WHERE s.external_id = $1
-`
-
 // GetAnalysisIDByExternalID returns the analysis ID based on the external ID
 // passed in.
 func (d *Database) GetAnalysisIDByExternalID(context context.Context, externalID string) (string, error) {
 	var analysisID string
 
+	const analysisIDByExternalIDQuery = `
+		SELECT j.id
+		FROM jobs j
+		JOIN job_steps s ON s.job_id = j.id
+		WHERE s.external_id = $1
+	`
+
 	err := d.db.QueryRowxContext(context, analysisIDByExternalIDQuery, externalID).Scan(&analysisID)
 	if err != nil {
 		return "", err
 	}
+
 	return analysisID, nil
 }
 
-const analysisQuery = `
-	SELECT
-		j.id,
-		j.app_id,
-		j.start_date,
-		j.end_date,
-		j.status,
-		j.deleted,
-		j.submission,
-		j.user_id,
-		j.subdomain,
-		t.name job_type,
-		t.system_id
-	FROM jobs j
-	JOIN job_types t ON j.job_type_id = job_types.id
-	WHERE j.id = $1
-	AND j.user_id = $2;
-`
-
 func (d *Database) Analysis(context context.Context, userID, id string) (*Analysis, error) {
 	var analysis Analysis
+
+	const analysisQuery = `
+		SELECT
+			j.id,
+			j.app_id,
+			j.start_date,
+			j.end_date,
+			j.status,
+			j.deleted,
+			j.submission,
+			j.user_id,
+			j.subdomain,
+			t.name job_type,
+			t.system_id
+		FROM jobs j
+		JOIN job_types t ON j.job_type_id = job_types.id
+		WHERE j.id = $1
+		AND j.user_id = $2;
+	`
+
 	err := d.db.QueryRowxContext(context, analysisQuery, id, userID).StructScan(&analysis)
+
 	return &analysis, err
 }
