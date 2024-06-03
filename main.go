0cf25fcbba99cede2b5b1919c6f2ea6cc5bf8d6f
Sarah Roberts <sonoran.sarah@gmail.com>
2024-06-03 16:54:23 -0700

    removed the check for the existence of the dotenv file

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -103,10 +102,6 @@ func main() {
 	log.Infof("NATS CA cert file is %s", *caCert)
 	log.Infof("NATS creds file is %s", *credsPath)
 	log.Infof("dotenv file is %s", *dotEnvPath)
-
-	if _, err = os.Open(*dotEnvPath); err != nil {
-		log.Fatal(err)
-	}
 
 	config, err = cfg.Init(&cfg.Settings{
 		EnvPrefix:   *envPrefix,
